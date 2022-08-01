1c5c7882be29133115bc0fcbc9c0946c10cb5ac0
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-08-01 14:28:58 -0500

    v0.5.0 Se integra menu en seccion

--- templates/directivas/adm_seccion_html.php
+++ templates/directivas/adm_seccion_html.php
@@ -1,14 +1,55 @@
 <?php
 namespace html;
 
+use gamboamartin\acl\controllers\controlador_adm_seccion;
 use gamboamartin\errores\errores;
 use gamboamartin\system\html_controler;
 use models\adm_seccion;
 use PDO;
+use stdClass;
 
 
 class adm_seccion_html extends html_controler {
 
+    private function asigna_inputs(controlador_adm_seccion $controler, stdClass $inputs): array|stdClass
+    {
+        $controler->inputs->select = new stdClass();
+
+        $controler->inputs->select->adm_menu_id = $inputs->selects->adm_menu_id;
+
+
+        return $controler->inputs;
+    }
+
+
+
+    public function genera_inputs_alta(controlador_adm_seccion $controler,PDO $link): array|stdClass
+    {
+        $inputs = $this->init_alta(link: $link);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar inputs',data:  $inputs);
+
+        }
+        $inputs_asignados = $this->asigna_inputs(controler:$controler, inputs: $inputs);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al asignar inputs',data:  $inputs_asignados);
+        }
+
+        return $inputs_asignados;
+    }
+
+    private function init_alta(PDO $link): array|stdClass
+    {
+        $selects = $this->selects_alta(link: $link);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar selects',data:  $selects);
+        }
+
+        $alta_inputs = new stdClass();
+        $alta_inputs->selects = $selects;
+
+        return $alta_inputs;
+    }
 
     public function select_adm_seccion_id(int $cols, bool $con_registros, int $id_selected, PDO $link): array|string
     {
@@ -19,6 +60,23 @@ class adm_seccion_html extends html_controler {
             return $this->error->error(mensaje: 'Error al generar select', data: $select);
         }
         return $select;
+    }
+
+    private function selects_alta(PDO $link): array|stdClass
+    {
+        $selects = new stdClass();
+
+
+
+        $select = (new adm_menu_html(html: $this->html_base))->select_adm_menu_id(cols: 12,
+            con_registros:false, id_selected:-1,link: $link);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar select',data:  $select);
+
+        }
+        $selects->adm_menu_id = $select;
+
+        return $selects;
     }
 
 }
