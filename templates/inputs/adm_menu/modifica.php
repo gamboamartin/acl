20e794f04396563b9a042ace3c6bc97c5db97620
mgamboa <gamboa.martin@gmail.com>
2022-11-11 17:30:23 -0600

    v0.72.1 Se implementa menu

--- templates/inputs/adm_menu/modifica.php
+++ templates/inputs/adm_menu/modifica.php
@@ -1,15 +1,8 @@
 <?php /** @var controllers\controlador_org_empresa $controlador  controlador en ejecucion */ ?>
 <?php use config\views; ?>
-<?php echo $controlador->forms_inputs_modifica; ?>
+<?php echo $controlador->inputs->codigo; ?>
+<?php echo $controlador->inputs->descripcion; ?>
 
 
-<?php echo $controlador->inputs->select->adm_menu_id; ?>
+<?php include (new views())->ruta_templates.'botons/submit/modifica_bd.php';?>
 
-
-<?php include (new views())->ruta_templates.'botons/submit/alta_bd_otro.php';?>
-
-<div class="control-group btn-alta col-12">
-    <div class="controls">
-        <?php include 'templates/botons/adm_menu_alta.php';?>
-    </div>
-</div>
