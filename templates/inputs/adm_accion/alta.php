3f419d0c816aedeebd96a2bd0b782d21e6eb8f64
mgamboa <gamboa.martin@gmail.com>
2022-11-22 14:45:12 -0600

    v0.98.4 se inetgra tranmsaccion alta de accion

--- templates/inputs/adm_accion/alta.php
+++ templates/inputs/adm_accion/alta.php
@@ -1,6 +1,6 @@
 <?php /** @var gamboamartin\acl\controllers\controlador_adm_seccion $controlador  controlador en ejecucion */ ?>
 <?php use config\views; ?>
-<?php echo $controlador->inputs->codigo; ?>
+
 <?php echo $controlador->inputs->descripcion; ?>
 
 <?php echo $controlador->inputs->titulo; ?>
--- templates/inputs/adm_seccion/modifica.php
+++ templates/inputs/adm_seccion/modifica.php
@@ -1,4 +1,4 @@
-<?php /** @var controllers\controlador_org_empresa $controlador  controlador en ejecucion */ ?>
+<?php /** @var gamboamartin\controllers\controlador_adm_seccion $controlador  controlador en ejecucion */ ?>
 <?php use config\views; ?>
 <?php echo $controlador->inputs->codigo; ?>
 <?php echo $controlador->inputs->descripcion; ?>
@@ -7,7 +7,7 @@
 <?php echo $controlador->inputs->adm_menu_id; ?>
 
 
-<?php include (new views())->ruta_templates.'botons/submit/alta_bd_otro.php';?>
+<?php include (new views())->ruta_templates.'botons/submit/modifica_bd.php';?>
 
 <div class="col-row-12">
     <?php foreach ($controlador->buttons as $button){ ?>
