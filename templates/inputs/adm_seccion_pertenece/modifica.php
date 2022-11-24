b674334de497f58aa69486faa70ab5b508b56ac2
mgamboa <gamboa.martin@gmail.com>
2022-11-24 09:27:36 -0600

    v0.109.5 Se ajusta seccion pertenece

---
+++ templates/inputs/adm_seccion_pertenece/modifica.php
@@ -0,0 +1,17 @@
+<?php /** @var gamboamartin\acl\controllers\controlador_adm_seccion $controlador  controlador en ejecucion */ ?>
+<?php use config\views; ?>
+
+
+<?php echo $controlador->inputs->adm_menu_id; ?>
+<?php echo $controlador->inputs->adm_seccion_id; ?>
+<?php echo $controlador->inputs->adm_sistema_id; ?>
+
+
+
+<?php include (new views())->ruta_templates.'botons/submit/modifica_bd.php';?>
+
+<div class="col-row-12">
+    <?php foreach ($controlador->buttons as $button){ ?>
+        <?php echo $button; ?>
+    <?php }?>
+</div>
