cf008bd8116280d99fd23f9b982d5c968d7559ef
mgamboa <gamboa.martin@gmail.com>
2024-01-03 14:48:01 -0600

    v15.2.1 FIX atributo vendor ruta

--- templates/inputs/adm_grupo/modifica.php
+++ templates/inputs/adm_grupo/modifica.php
@@ -1,3 +1,3 @@
 <?php /** @var gamboamartin\acl\controllers\controlador_adm_grupo $controlador  controlador en ejecucion */ ?>
 <?php use config\generales; ?>
-<?php include (new generales())->path_base.$controlador->ruta_vendero_acl.'templates/inputs/_parent/modifica.php'; ?>
+<?php include (new generales())->path_base.$controlador->ruta_vendor_acl.'templates/inputs/_parent/modifica.php'; ?>
